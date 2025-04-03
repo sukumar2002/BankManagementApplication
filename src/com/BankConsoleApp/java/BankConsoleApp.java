39377ad2a374c28c7814216e9e944d4ecfc91862
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-04-03 21:39:41 +0530

    Update BankConsoleApp.java

--- src/com/BankConsoleApp/java/BankConsoleApp.java
+++ src/com/BankConsoleApp/java/BankConsoleApp.java
@@ -1,23 +1,18 @@
 package com.BankConsoleApp.java;
-
-
 import com.AccountType.java.AccountType;
 import com.BankService.java.BankService;
-
 import java.util.Scanner;
 
 public class BankConsoleApp {
     public static void run() {
         BankService bankService = new BankService();
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
             System.out.println("\nWelcome to Bank System");
             System.out.println("1. Create Account");
             System.out.println("2. Exit");
             System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
-
             switch (choice) {
                 case 1:
                     System.out.print("Enter Account Number: ");

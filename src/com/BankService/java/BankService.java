bc68a5f290eee48b02b7138b1ffda2bc4dbfd0bb
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-04-03 21:39:08 +0530

    Update BankService.java

--- src/com/BankService/java/BankService.java
+++ src/com/BankService/java/BankService.java
@@ -1,13 +1,10 @@
 package com.BankService.java;
-
-
 import com.AccountRepository.java.AccountRepository;
 import com.AccountType.java.AccountType;
 import com.BankAccountFactory.java.BankAccountFactory;
 
 public class BankService {
     private final AccountRepository accountRepository = new AccountRepository();
-
     public void addAccount(int accNo, String holder, double balance, AccountType type) {
         com.bank.management.model.Account account = BankAccountFactory.createAccount(accNo, holder, balance, type);
         accountRepository.saveAccount(account);
